9123efeabb507b5e5c030d9410d553eda895a66a
assembleiasmb <assembleiasmb@gmail.com>
2026-02-17 16:54:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3764856192-b8f9f",
+  "appId": "1:174088894972:web:36f170c127a21dddb55a79",
+  "apiKey": "AIzaSyDTuHnisxetF4HnCsCa8eiU97iXyYd_0xA",
+  "authDomain": "studio-3764856192-b8f9f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "174088894972"
+};
