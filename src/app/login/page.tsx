2a05a0e67c115ca5537075f88cb4899fd859045e
assembleiasmb <assembleiasmb@gmail.com>
2026-02-20 03:21:24 +0000

    Consegue ocultar o botão login com o google so de celulares, deixando o

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -332,20 +332,24 @@ export default function LoginPage() {
                     {isLoadingEmail ? <Loader2 className="h-4 w-4 animate-spin" /> : "Entrar / Cadastrar"}
                 </Button>
             </form>
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <span className="w-full border-t" />
+            {!isMobile && (
+              <>
+                <div className="relative">
+                  <div className="absolute inset-0 flex items-center">
+                    <span className="w-full border-t" />
+                  </div>
+                  <div className="relative flex justify-center text-xs uppercase">
+                    <span className="bg-background px-2 text-muted-foreground">
+                      Ou
+                    </span>
+                  </div>
                 </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span className="bg-background px-2 text-muted-foreground">
-                    Ou
-                  </span>
-                </div>
-              </div>
-            <Button variant="outline" className="w-full" type="button" disabled={isLoading} onClick={handleGoogleLogin}>
-                {isLoadingGoogle ? <Loader2 className="h-4 w-4 animate-spin" /> : <Icons.google className="h-4 w-4" />}
-                Entrar com Google
-              </Button>
+                <Button variant="outline" className="w-full" type="button" disabled={isLoading} onClick={handleGoogleLogin}>
+                    {isLoadingGoogle ? <Loader2 className="h-4 w-4 animate-spin" /> : <Icons.google className="h-4 w-4" />}
+                    Entrar com Google
+                  </Button>
+              </>
+            )}
           </CardContent>
         </Card>
     </div>
